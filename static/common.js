3f7c886f76fa54ad5d8aeb50bbb098767eec389f
darkflame91 <darkflame91@gmail.com>
2022-08-13 15:29:48 +0530

    Handle disabled js and support smaller screens

--- static/common.js
+++ static/common.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
 
     if(Cookies.get('covers')==undefined){
-        Cookies.set('covers', true, { expires: 365 });
-        Cookies.set('format', 'mobi', { expires: 365 });
-        Cookies.set('language', 'English', { expires: 365 });
+        Cookies.set('covers', true, { expires: 3650 });
+        Cookies.set('format', 'mobi', { expires: 3650 });
+        Cookies.set('language', 'English', { expires: 3650 });
     }
-    
-    console.log("COOKIEEE  " + Cookies.get('covers'));
-    console.log("COOKIEEE  " + Cookies.get('format'));
-    console.log("COOKIEEE  " + Cookies.get('language'));
 
     $('.fitbox').boxfit({multiline: true});
 
